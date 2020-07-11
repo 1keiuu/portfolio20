03f6bdd04f722e044fe5a1225c6a93ab09a767f7
ikkei12 <ikkei12.h@gmail.com>
2020-07-12 00:31:35 +0900

    週別/月別で集計

--- front/app/src/pages/skill/Graph.tsx
+++ front/app/src/pages/skill/Graph.tsx
@@ -1,33 +1,8 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 import "./style/Graph.scss";
+import moment from "moment";
 
-const weeklyData = {
-  labels: ["7/7", "7/6", "7/5", "7/4", "7/3", "7/2", "7/1"],
-  datasets: [
-    {
-      label: "My First dataset",
-      fill: true,
-      lineTension: 0.1,
-      backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
-      borderCapStyle: "round",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "square",
-      pointBorderColor: "rgba(75,192,192,1)",
-      pointBackgroundColor: "#eee",
-      pointBorderWidth: 10,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 1,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [3, 10, 21, 31, 34, 40, 48],
-    },
-  ],
-};
 const monthlyData = {
   labels: ["7月", "6月", "5月", "4月", "3月", "2月", "1月"],
   datasets: [
@@ -58,7 +33,7 @@ const yearlyData = {
   labels: ["2020", "2019", "2018", "2017", "2016", "2015", "2014"],
   datasets: [
     {
-      label: "My First dataset",
+      label: "1年間のGitHub contribution",
       fill: true,
       lineTension: 0.1,
       backgroundColor: "rgba(75,192,192,0.4)",
@@ -85,19 +60,124 @@ interface Props {
 }
 interface State {
   data: object;
+  weeklyData: {
+    labels: string[];
+    datasets: {
+      label: string;
+      fill: boolean;
+      lineTension: number;
+      backgroundColor: string;
+      borderColor: string;
+      borderCapStyle: string;
+      borderDash: never[];
+      borderDashOffset: number;
+      borderJoinStyle: string;
+      pointBorderColor: string;
+      pointBackgroundColor: string;
+      pointBorderWidth: number;
+      pointHoverRadius: number;
+      pointHoverBackgroundColor: string;
+      pointHoverBorderColor: string;
+      pointHoverBorderWidth: number;
+      pointRadius: number;
+      pointHitRadius: number;
+      data: any;
+    }[];
+  };
+  monthlyData: object;
+  yearlyData: object;
 }
+const contributionsData = (labels: string[], data: number[]) => {
+  return {
+    labels: labels,
+    datasets: [
+      {
+        label: "直近1週間のGitHub contribution",
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "round",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "square",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#eee",
+        pointBorderWidth: 10,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 1,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: data,
+      },
+    ],
+  };
+};
 export default class Graph extends React.Component<Props, State> {
+  state: State = {
+    data: {},
+    weeklyData: contributionsData([], []),
+    monthlyData: {},
+    yearlyData: {},
+  };
+
+  getWeeklyData(data: []) {
+    const today: object | undefined = data.find((d: { date: string }) => {
+      return (
+        d.date == moment(new Date()).subtract(1, "day").format("YYYY-MM-DD")
+      );
+    });
+    if (today) {
+      const todayIndex = data.indexOf(today);
+      const LABELS = data
+        .slice(todayIndex, todayIndex + 14)
+        .map((d: { date: string }) => {
+          return moment(d.date).format("MM/DD");
+        })
+        .reverse();
+      const DATA = data
+        .slice(todayIndex, todayIndex + 14)
+        .map((d: { count: number }) => {
+          return d.count;
+        })
+        .reverse();
+
+      return { labels: LABELS, data: DATA };
+    } else {
+      return { labels: [], data: [] };
+    }
+  }
+
+  getMonthlyData(data: []) {
+    const today: {}[] | undefined = data.filter((d: { date: string }) => {
+      return (
+        d.date == moment(new Date()).subtract(1, "day").format("YYYY-MM-DD")
+      );
+    });
+    console.log(data);
+    data.forEach(() => {});
+  }
+
   componentDidMount() {
     this.props.contributionsPromise.then((data) => {
       console.log(data);
+      // const weekly: { labels: string[]; data: number[] } = this.getWeeklyData(
+      //   data
+      // );
+      // this.setState({
+      //   weeklyData: contributionsData(weekly.labels, weekly.data),
+      // });
+      // this.getMonthlyData(data);
+      // this.setState({ data: this.state.weeklyData });
     });
-    this.setState({ data: weeklyData });
   }
   dataChange = (e: any) => {
     const val = e.target.value;
     switch (val) {
       case "week":
-        this.setState({ data: weeklyData });
+        // this.setState({ data: weeklyData });
         break;
       case "month":
         this.setState({ data: monthlyData });
@@ -115,7 +195,7 @@ export default class Graph extends React.Component<Props, State> {
           <option value="month">Month</option>
           <option value="year">Year</option>
         </select>
-        <Line data={yearlyData} redraw={true} />
+        <Line data={this.state.data} redraw={true} />
       </div>
     );
   }
--- web/app/controllers/contributions_controller.rb
+++ web/app/controllers/contributions_controller.rb
@@ -1,5 +1,41 @@
 class ContributionsController < BaseController
     def index
-        render :json => {contributions: Contribution.all.order(date: "DESC").map{|contribute|{count: contribute.count,date: contribute.date}}}
+        contributions_array = Contribution.all.to_a
+
+        today  = Time.current.at_beginning_of_day
+
+        # 週別で取得
+        lastday_of_thisweek = (today - 14.day)
+        lastday_of_thisyear = (today - 1.year)
+        contribution_of_thisyear = contributions_array.select{|d|today >d.date && d.date >lastday_of_thisyear}
+        weekly = contribution_of_thisyear.select{|y|today >y.date && y.date >=lastday_of_thisweek}
+        
+        # 月別で取得
+        monthly = []
+        i = today.month
+        while i >= 1
+            if i == today.month
+                monthly_data = contribution_of_thisyear.select{|y| y.date.month == i && y.date.year == today.year}
+            else
+                monthly_data = contribution_of_thisyear.select{|y| y.date.month == i}
+            end
+            monthly.push({month:monthly_data[0]&.date&.strftime("%Y/%m"),count_sum:monthly_data.pluck(:count).sum()})
+            i -= 1
+        end
+        index = 12
+        diff = today.month + 1
+        while index >= diff
+            if diff == today.month
+                monthly_data = contribution_of_thisyear.select{|y| y.date.month == index && y.date.year == today.year}
+            else
+                monthly_data = contribution_of_thisyear.select{|y| y.date.month == index}
+            end
+            monthly.push({month:monthly_data[0].date.strftime("%Y/%m"),count_sum:monthly_data.pluck(:count).sum()})
+            index -= 1
+        end
+
+        # 年別で取得
+            
+        render :json => {contributions:{weekly:weekly,monthly:monthly}}
     end
 end
