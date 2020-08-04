162acdc9a4fa9989e36070974c50ead90e32fc2f
ikkei12 <ikkei12.h@gmail.com>
2020-08-04 20:40:53 +0900

    connect front with backend

--- front/app/src/pages/home/Home.tsx
+++ front/app/src/pages/home/Home.tsx
@@ -1,10 +1,40 @@
 import React from "react";
+import axios from "axios";
+
 const Fade = require("react-reveal/Fade");
 export default class Home extends React.Component {
+  getData = async () => {
+    const request = axios.create({
+      baseURL: `${process.env.REACT_APP_API_URL}`,
+    });
+    console.log(`${process.env.REACT_APP_API_URL}`);
+    await request
+      .get("/contributions", {
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+    // const URL = "/contributions";
+    // const res = await axios({
+    //   method: "get",
+    //   url: "contributions",
+    //   baseURL: `${process.env.REACT_APP_API_URL}`,
+    //   headers: { "Content-Type": "application/json" },
+    // });
+  };
+
   render() {
     return (
       <Fade bottom>
         <p>home</p>
+        <button
+          onClick={async () => {
+            await this.getData();
+          }}
+        >
+          click
+        </button>
       </Fade>
     );
   }
