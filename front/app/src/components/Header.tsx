08c190afc6e929434f2ee25ad2ab3f4d0bc46af2
ikkei12 <ikkei12.h@gmail.com>
2020-08-07 00:02:19 +0900

    add style to Header

--- front/app/src/components/Header.tsx
+++ front/app/src/components/Header.tsx
@@ -1,75 +1,104 @@
 import React, { useState } from "react";
-import { Link, RouteComponentProps } from "react-router-dom";
-import MenuIcon from "./MenuIcon";
-import ArrowIcon from "./ArrowIcon";
+import { Link } from "react-router-dom";
 import "../styles/header.scss";
 import { CSSTransition } from "react-transition-group";
 
 type Props = {};
 
-// const MenuButton = (props: any) => {
-//   if (!props.isMenuOpen) {
-//     return (
-//       <div
-//         className="menu-icon__wrapper"
-//         onClick={() => {
-//           props.click();
-//         }}
-//       >
-//         <MenuIcon />
-//       </div>
-//     );
-//   } else {
-//     return (
-//       <div
-//         className="close-button"
-//         onClick={() => {
-//           props.click();
-//         }}
-//       >
-//         <div className="close-button__inner">
-//           <p>Close</p>
-//           <div className="close-icon__wrapper">
-//             <ArrowIcon isLeft={false} fill="#f8f2ee" />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// };
 const Header: React.FC<Props> = (props) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const changeMenuStatus = (type: string) => {
-    if (type === "open") {
-      setIsMenuOpen(true);
-    } else {
-      setIsMenuOpen(false);
-    }
-  };
+  const [selectedItem, setSelectedItem] = useState("");
 
   return (
-    <header className={"header" + " " + (isMenuOpen ? "--is-menu-open" : "")}>
+    <header>
       <div className="menu-items">
-        <Link to="/" className="menu-item">
-          Home
-        </Link>
-        <Link to="/profile" className="menu-item">
-          Profile
-        </Link>
-        <Link to="/product" className="menu-item">
-          Product
-        </Link>
-        <Link to="/contact" className="menu-item">
-          Contact
-        </Link>
+        <div className="menu-item__wrapper">
+          <Link
+            to="/"
+            className="menu-item"
+            onMouseEnter={() => {
+              setSelectedItem("home");
+            }}
+            onMouseLeave={() => {
+              setSelectedItem("");
+            }}
+          >
+            Home
+          </Link>
+          <CSSTransition
+            in={selectedItem == "home"}
+            classNames="underline"
+            timeout={0}
+          >
+            <div className="underline"></div>
+          </CSSTransition>
+        </div>
+
+        <div className="menu-item__wrapper">
+          <Link
+            to="/profile"
+            className="menu-item"
+            onMouseEnter={() => {
+              setSelectedItem("profile");
+            }}
+            onMouseLeave={() => {
+              setSelectedItem("");
+            }}
+          >
+            Profile
+          </Link>
+          <CSSTransition
+            in={selectedItem == "profile"}
+            classNames="underline"
+            timeout={0}
+          >
+            <div className="underline"></div>
+          </CSSTransition>
+        </div>
+
+        <div className="menu-item__wrapper">
+          <Link
+            to="/product"
+            className="menu-item"
+            onMouseEnter={() => {
+              setSelectedItem("product");
+            }}
+            onMouseLeave={() => {
+              setSelectedItem("");
+            }}
+          >
+            Product
+          </Link>
+          <CSSTransition
+            in={selectedItem == "product"}
+            classNames="underline"
+            timeout={0}
+          >
+            <div className="underline"></div>
+          </CSSTransition>
+        </div>
+
+        <div className="menu-item__wrapper">
+          <Link
+            to="/contact"
+            className="menu-item"
+            onMouseEnter={() => {
+              setSelectedItem("contact");
+            }}
+            onMouseLeave={() => {
+              setSelectedItem("");
+            }}
+          >
+            Contact
+          </Link>
+          <CSSTransition
+            in={selectedItem == "contact"}
+            classNames="underline"
+            timeout={0}
+          >
+            <div className="underline"></div>
+          </CSSTransition>
+        </div>
       </div>
-      <CSSTransition
-        in={isMenuOpen}
-        classNames="menu-items__underline"
-        timeout={0}
-      >
-        <div className="menu-items__underline"></div>
-      </CSSTransition>
     </header>
   );
 };
