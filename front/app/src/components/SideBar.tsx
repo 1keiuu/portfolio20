d69fd0f1f8f1eb23ea35b9140347fcfa1616591b
ikkei12 <ikkei12.h@gmail.com>
2020-08-07 16:11:52 +0900

    fix SideBar style

--- front/app/src/components/SideBar.tsx
+++ front/app/src/components/SideBar.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "../styles/sidebar.scss";
-import { RouteComponentProps } from "react-router-dom";
 import PageTitle from "./PageTitle";
+import igIcon from "../images/ig-icon.png";
+import wantedlyIcon from "../images/wantedly_mark.png";
+import gitIcon from "../images/GitHub_Icon.png";
 
 type Props = {
   current_page: string;
@@ -10,7 +12,32 @@ type Props = {
 const SideBar: React.FC<Props> = (props) => {
   return (
     <div className="sidebar">
-      <div className="sidebar__inner"></div>
+      <div className="sidebar__inner">
+        <p className="my-name">Ikkei Harashima</p>
+        <div className="icon__group">
+          <a
+            href="https://github.com/ikkei12"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={gitIcon} className="git-icon" />
+          </a>
+          <a
+            href="https://www.instagram.com/1keiuu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={igIcon} className="ig-icon" />
+          </a>
+          <a
+            href=" https://www.wantedly.com/users/103088073"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={wantedlyIcon} className="wantedly-icon" />
+          </a>
+        </div>
+      </div>
       <div className="page-title__wrapper">
         <PageTitle
           title={props.current_page ? props.current_page : "WELCOME"}
