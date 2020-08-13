cd87bc5dcd34167e6f8b80b0063b318d0545e9cf
ikkei12 <ikkei12.h@gmail.com>
2020-08-13 12:24:41 +0900

    fix slider css

--- front/app/src/components/ProductCard.tsx
+++ front/app/src/components/ProductCard.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import "../styles/productCard.scss";
 interface Product {
   title: string;
+  span: string;
+  imageUrl: string;
   backgroundColor: string;
 }
 
 interface Props {
   product: Product;
+  isHover: boolean;
 }
 
 const ProductCard: React.FC<Props> = (props) => {
@@ -16,8 +19,23 @@ const ProductCard: React.FC<Props> = (props) => {
       style={{ background: props.product.backgroundColor }}
     >
       <div className="product-card__inner">
-        <div className="product-card__title-wrapper">
-          <p>{props.product.title}</p>
+        <div className="product-card__image__wrapper">
+          <img
+            className={
+              "product-card__image" + " " + (props.isHover ? "--hover" : "")
+            }
+            src={props.product.imageUrl}
+          ></img>
+        </div>
+        <div
+          className={
+            "product-card__title-wrapper" +
+            " " +
+            (props.isHover ? "--hover" : "")
+          }
+        >
+          <p className="product-card__title">{props.product.title}</p>
+          <p className="product-card__sub-title">{props.product.span}</p>
         </div>
       </div>
     </div>
--- front/app/src/pages/product/ProductPage.tsx
+++ front/app/src/pages/product/ProductPage.tsx
@@ -14,16 +14,48 @@ const Fade = require("react-reveal/Fade");
 SwiperCore.use([Mousewheel]);
 
 const products = [
-  { title: "test", backgroundColor: "#7996BF" },
-  { title: "test2", backgroundColor: "#658378" },
-  { title: "test3", backgroundColor: "#EF6D6D" },
-  { title: "test4", backgroundColor: "#7996BF" },
-  { title: "test5", backgroundColor: "#658378" },
-  { title: "test6", backgroundColor: "#EF6D6D" },
+  {
+    title: "test",
+    span: "2020/02~2020/07",
+    backgroundColor: "#7996BF",
+    imageUrl: "https://loremflickr.com/640/360",
+  },
+  {
+    title: "test2",
+    span: "2020/02~2020/07",
+    backgroundColor: "#658378",
+    imageUrl: "https://loremflickr.com/640/360",
+  },
+  {
+    title: "test3",
+    span: "2020/02~2020/07",
+    backgroundColor: "#EF6D6D",
+    imageUrl: "https://loremflickr.com/640/360",
+  },
+  {
+    title: "test4",
+    span: "2020/02~2020/07",
+    backgroundColor: "#7996BF",
+    imageUrl: "https://loremflickr.com/640/360",
+  },
+  {
+    title: "test5",
+    span: "2020/02~2020/07",
+    backgroundColor: "#658378",
+    imageUrl: "https://loremflickr.com/640/360",
+  },
+  {
+    title: "test6",
+    span: "2020/02~2020/07",
+    backgroundColor: "#EF6D6D",
+    imageUrl: "https://loremflickr.com/640/360",
+  },
 ];
 
 const ProductPage: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+
   return (
     <Fade bottom delay={500}>
       <div className="product-page">
@@ -31,17 +63,26 @@ const ProductPage: React.FC = () => {
           <VerticalSlider>
             <Swiper
               direction="vertical"
-              slidesPerView={1}
+              slidesPerView={2}
               speed={1000}
-              spaceBetween={60}
+              // spaceBetween={60}
               mousewheel={true}
               onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}
             >
               {products.map((product, i) => {
                 return (
-                  <SwiperSlide key={"product-slide" + i}>
+                  <SwiperSlide
+                    key={"product-slide" + i}
+                    onMouseEnter={() => {
+                      setHoveredIndex(i);
+                    }}
+                    onMouseLeave={() => {
+                      setHoveredIndex(-1);
+                    }}
+                  >
                     <ProductCard
                       product={product}
+                      isHover={i == hoveredIndex}
                       key={"product" + i}
                     ></ProductCard>
                   </SwiperSlide>
