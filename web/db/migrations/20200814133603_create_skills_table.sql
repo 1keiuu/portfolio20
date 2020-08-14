3a3bb242b6694352620ceacae6b7a15ee8213e4a
ikkei12 <ikkei12.h@gmail.com>
2020-08-14 22:37:53 +0900

    create skills table

--- web/db/migrations/20200814133603_create_skills_table.sql
+++ web/db/migrations/20200814133603_create_skills_table.sql
@@ -1,8 +1,16 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-
+CREATE TABLE skills(
+    id int AUTO_INCREMENT,
+    name varchar(255),
+    skill_type_id int,
+    PRIMARY KEY(id),
+    FOREIGN KEY(skill_type_id)
+    REFERENCES skill_types(id)
+);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
+DROP TABLE skills;
