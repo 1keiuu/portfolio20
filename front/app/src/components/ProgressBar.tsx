50615d4e48cac46b837eb5f3ecb0bbc691eb020f
ikkei12 <ikkei12.h@gmail.com>
2020-08-18 21:57:07 +0900

    fix progressBar style

--- front/app/src/components/ProgressBar.tsx
+++ front/app/src/components/ProgressBar.tsx
@@ -4,32 +4,60 @@ import "../styles/progreeBar.scss";
 interface Props {
   active_index: number;
 }
+function usePrevious(value: any) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const ProgreeBar: React.FC<Props> = (props) => {
   const REF = useRef<HTMLDivElement>(null);
   const [currentLeft, setCurrentLeft] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const prevIndex = usePrevious(activeIndex);
+
   useEffect(() => {
     const currentDot = REF.current!.children[props.active_index] as HTMLElement;
     setCurrentLeft(currentDot!.offsetLeft);
+    if (props.active_index > prevIndex!) {
+      // バーが戻る時は遅らせたくない
+      setTimeout(() => {
+        setActiveIndex(props.active_index);
+      }, 900);
+    } else {
+      setActiveIndex(props.active_index);
+    }
   }, [props.active_index]);
   return (
     <div className="progress-bar" ref={REF}>
-      <span className={"progress-bar__dot --active"}></span>
+      <span className={"dot__border --active"}>
+        <span className={"dot --active"}></span>
+      </span>
       <span
-        className={
-          "progress-bar__dot" + " " + (props.active_index > 0 ? "--active" : "")
-        }
-      ></span>
+        className={"dot__border" + " " + (activeIndex > 0 ? "--active" : "")}
+      >
+        <span
+          className={"dot" + " " + (activeIndex > 0 ? "--active" : "")}
+        ></span>
+      </span>
       <span
-        className={
-          "progress-bar__dot" + " " + (props.active_index > 1 ? "--active" : "")
-        }
-      ></span>
+        className={"dot__border" + " " + (activeIndex > 1 ? "--active" : "")}
+      >
+        <span
+          className={"dot" + " " + (activeIndex > 1 ? "--active" : "")}
+        ></span>
+      </span>
+
       <span
-        className={
-          "progress-bar__dot" + " " + (props.active_index > 2 ? "--active" : "")
-        }
-      ></span>
+        className={"dot__border" + " " + (activeIndex > 2 ? "--active" : "")}
+      >
+        <span
+          className={"dot" + " " + (activeIndex > 2 ? "--active" : "")}
+        ></span>
+      </span>
+
       <span className="progress-bar__line"></span>
       <span
         className="progress-bar__active-line"
