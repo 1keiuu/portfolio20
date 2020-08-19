1b2820ee222ded9e9542d44718593e3d3cd0a1b9
ikkei12 <ikkei12.h@gmail.com>
2020-08-19 12:31:08 +0900

    add profile layout

--- front/app/src/pages/profile/ProfileSlide.tsx
+++ front/app/src/pages/profile/ProfileSlide.tsx
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/profileSlide.scss";
+import profileImg from "../../images/profile.jpeg";
+import { CSSTransition } from "react-transition-group";
 const Fade = require("react-reveal/Fade");
 
 const ProfileSlide: React.FC = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
   return (
     <div className="slide profile__slide">
-      <Fade bottom>
-        <div>
-          <img></img>
-          <div>
-            <p>
-              テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-            </p>
-            <p>
-              テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-            </p>
-          </div>
+      <div className="slide-upper">
+        <div className="slide-upper__image-wrapper">
+          <CSSTransition
+            in={isLoaded}
+            classNames="slide-upper__image-surface"
+            timeout={500}
+          >
+            <div className="slide-upper__image-surface"></div>
+          </CSSTransition>
+          <img className="slide-upper__image" src={profileImg}></img>
+          <div className="noise --upper"></div>
         </div>
-        <div>
+        <div className="slide-upper__text-group">
+          <p>
+            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+          </p>
           <p>
             テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
           </p>
         </div>
-      </Fade>
+      </div>
+      <div className="slide-lower">
+        <p>
+          テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+          テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+        </p>
+      </div>
     </div>
   );
 };
