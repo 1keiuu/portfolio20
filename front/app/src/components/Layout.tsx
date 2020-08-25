cd63d2e563be696429d2355778e8ee012b1089f0
ikkei12 <ikkei12.h@gmail.com>
2020-08-25 10:58:36 +0900

    fix product page

--- front/app/src/components/Layout.tsx
+++ front/app/src/components/Layout.tsx
@@ -1,177 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../store";
-import { addProductsAction } from "../store/product/actions";
-import { addSkillsAction } from "../store/skill/actions";
-
 import Header from "./Header";
 import Sidebar from "./SideBar";
-import {
-  BrowserRouter as Router,
-  Route,
-  RouteComponentProps,
-  useRouteMatch,
-} from "react-router-dom";
-import axios from "axios";
 import "../styles/layout.scss";
-import Home from "../pages/home/Home";
-import ProfilePage from "../pages/profile/ProfilePage";
-import ProductPage from "../pages/product/ProductPage";
-import ContactPage from "../pages/contact/ContactPage";
 import SlideCurtain from "../components/SlideCurtain";
-import SearchProductBar from "../components/SearchProductBar";
-import GopherImage from "../components/GopherImage";
 import { CSSTransition } from "react-transition-group";
 
 interface Props {
   current_page: string;
 }
 
-// const Inner = (props: { current_page: string; products: Product[] }) => {
-//   switch (props.current_page) {
-//     case "profile":
-//       return <ProfilePage></ProfilePage>;
-//     case "product":
-//       return <ProductPage products={props.products}></ProductPage>;
-//     case "contact":
-//       return <ContactPage></ContactPage>;
-//     default:
-//       return <Home></Home>;
-//   }
-// };
-
-interface Product {
-  id: number;
-  title: string;
-  span: string;
-  background_color: string;
-  images: string;
-  descriptions: string;
-  skill_ids: string;
-}
-
-interface Skill {
-  id: number;
-  name: string;
-  image_url: string;
-  background_color: string;
-  skill_type_name: string;
-}
-
-interface SearchProductProps {
-  current_page: string;
-  skills: {
-    skill_type_name: string;
-    id: number;
-    name: string;
-    background_color: string;
-    image_url: string;
-  }[];
-  change_products: (arr: Skill[]) => void;
-}
-
-const SearchProduct: React.FC<SearchProductProps> = (props) => {
-  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-  const handleDecideButtonClick = (selectedItems: Skill[]) => {
-    props.change_products(selectedItems);
-    setIsSideBarOpen(false);
-  };
-
-  if (props.current_page == "product") {
-    return (
-      <div className="search-product__wrapper">
-        <SearchProductBar
-          skills={props.skills}
-          isOpen={isSideBarOpen}
-          decide_button_callback={(selectedItems) => {
-            handleDecideButtonClick(selectedItems);
-          }}
-        ></SearchProductBar>
-        <GopherImage
-          callback={() => {
-            setIsSideBarOpen(!isSideBarOpen);
-          }}
-        ></GopherImage>
-        <div
-          onClick={() => {
-            setIsSideBarOpen(false);
-          }}
-          className={
-            "search-product__background" +
-            " " +
-            (isSideBarOpen ? "--active" : "")
-          }
-        ></div>
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
-};
-
 const Layout: React.FC<Props> = (props) => {
-  const storeProducts = useSelector((state: RootState) => state.product);
-  const storeSkills = useSelector((state: RootState) => state.skill);
-
-  const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [products, setProducts] = useState<Product[]>([
-    // {
-    //   id: 1,
-    //   title: "string",
-    //   span: "string",
-    //   background_color: "#333",
-    //   images: "string",
-    //   descriptions: "string",
-    //   skill_ids: "1,2",
-    // },
-    // {
-    //   id: 1,
-    //   title: "string",
-    //   span: "string",
-    //   background_color: "#333",
-    //   images: "string",
-    //   descriptions: "string",
-    //   skill_ids: "2,5",
-    // },
-    // {
-    //   id: 1,
-    //   title: "string",
-    //   span: "string",
-    //   background_color: "#333",
-    //   images: "string",
-    //   descriptions: "string",
-    //   skill_ids: "2,4",
-    // },
-  ]);
-  const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
-
-  // action を発行する関数
-  // 引数にはaction creatorを渡す
-  // 親のrenderごとに子のrenderが走るので、useCallbackを用いメモ化すべき。
-  const handleAddProducts = (products: Product[]) => {
-    dispatch(addProductsAction(products));
-  };
-  const handleAddSkills = (skills: any) => {
-    dispatch(addSkillsAction(skills));
-  };
-  let { path, url } = useRouteMatch();
 
   useEffect(() => {
-    console.log(path, url);
-    const PRODUCTS_URL = `${process.env.REACT_APP_API_URL}/api/products`;
-    const SKILLS_URL = `${process.env.REACT_APP_API_URL}/api/skills`;
-
-    const get = async () => {
-      await axios.get(PRODUCTS_URL).then((res) => {
-        handleAddProducts(res.data.products);
-        setSelectedProducts(res.data.products);
-      });
-      await axios.get(SKILLS_URL).then((res) => {
-        handleAddSkills(res.data.skills);
-      });
-    };
-    get();
     setIsLoaded(true);
   }, []);
   return (
@@ -185,19 +26,6 @@ const Layout: React.FC<Props> = (props) => {
             {props.children}
           </div>
         </div>
-        <SearchProduct
-          current_page={props.current_page}
-          skills={storeSkills.value}
-          change_products={(selectedItems: Skill[]) => {
-            selectedItems.forEach((item) => {
-              const newProducts = products.filter((product) => {
-                const arr = product.skill_ids.split(",");
-                return arr.includes(item.id.toString());
-              });
-              setSelectedProducts(newProducts);
-            });
-          }}
-        ></SearchProduct>
         <div className="noise"></div>
       </div>
     </CSSTransition>
--- front/app/src/pages/product/ProductPage.tsx
+++ front/app/src/pages/product/ProductPage.tsx
@@ -3,7 +3,16 @@ import VerticalSlider from "../../components/VerticalSlider";
 import ProductCard from "../../components/ProductCard";
 import SlidePagination from "../../components/SlidePagination";
 import "../../styles/productPage.scss";
+import axios from "axios";
+import SearchProductBar from "../../components/SearchProductBar";
+import GopherImage from "../../components/GopherImage";
 
+//redux
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../store";
+import { addProductsAction } from "../../store/product/actions";
+import { addSkillsAction } from "../../store/skill/actions";
+//swiper
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Mousewheel } from "swiper";
 import "swiper/swiper.scss";
@@ -20,11 +29,60 @@ interface Product {
   background_color: string;
   images: string;
   descriptions: string;
+  skill_ids: string;
 }
-interface Props {
-  products: Product[];
+
+interface Skill {
+  id: number;
+  name: string;
+  image_url: string;
+  background_color: string;
+  skill_type_name: string;
 }
+
+interface SearchProductProps {
+  current_page: string;
+  skills: {
+    skill_type_name: string;
+    id: number;
+    name: string;
+    background_color: string;
+    image_url: string;
+  }[];
+}
+interface Props {}
 const ProductPage: React.FC<Props> = (props) => {
+  const [products, setProducts] = useState<Product[]>([
+    // {
+    //   id: 1,
+    //   title: "string",
+    //   span: "string",
+    //   background_color: "#333",
+    //   images: "string",
+    //   descriptions: "string",
+    //   skill_ids: "1,2",
+    // },
+    // {
+    //   id: 1,
+    //   title: "string",
+    //   span: "string",
+    //   background_color: "#333",
+    //   images: "string",
+    //   descriptions: "string",
+    //   skill_ids: "2,5",
+    // },
+    // {
+    //   id: 1,
+    //   title: "string",
+    //   span: "string",
+    //   background_color: "#333",
+    //   images: "string",
+    //   descriptions: "string",
+    //   skill_ids: "2,4",
+    // },
+  ]);
+
+  //swiper
   const [currentIndex, setCurrentIndex] = useState(0);
   const [hoveredIndex, setHoveredIndex] = useState(-1);
   const [swiper, setSwiper] = useState({
@@ -35,10 +93,47 @@ const ProductPage: React.FC<Props> = (props) => {
   const changeCurrentSlide = (i: number) => {
     swiper.slideTo(i);
   };
-  // useEffect(() => {
-  //   const newProduct = [...props.products];
-  //   setProducts(newProduct);
-  // }, [props.products]);
+  //store
+  const storeProducts = useSelector((state: RootState) => state.product);
+  const storeSkills = useSelector((state: RootState) => state.skill);
+  const dispatch = useDispatch();
+  const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
+  const handleAddProducts = (products: Product[]) => {
+    dispatch(addProductsAction(products));
+  };
+  const handleAddSkills = (skills: any) => {
+    dispatch(addSkillsAction(skills));
+  };
+
+  // sidebar
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+  const handleDecideButtonClick = (selectedItems: Skill[]) => {
+    // 絞り込み機能
+    selectedItems.forEach((item) => {
+      const newProducts = products.filter((product) => {
+        const arr = product.skill_ids.split(",");
+        return arr.includes(item.id.toString());
+      });
+      setSelectedProducts(newProducts);
+    });
+    setIsSideBarOpen(false);
+  };
+
+  useEffect(() => {
+    const PRODUCTS_URL = `${process.env.REACT_APP_API_URL}/api/products`;
+    const SKILLS_URL = `${process.env.REACT_APP_API_URL}/api/skills`;
+
+    const get = async () => {
+      await axios.get(PRODUCTS_URL).then((res) => {
+        handleAddProducts(res.data.products);
+        setSelectedProducts(res.data.products);
+      });
+      await axios.get(SKILLS_URL).then((res) => {
+        handleAddSkills(res.data.skills);
+      });
+    };
+    get();
+  }, []);
   return (
     <Fade bottom delay={500}>
       <div className="product-page">
@@ -56,7 +151,7 @@ const ProductPage: React.FC<Props> = (props) => {
                 setCurrentIndex(swiper.activeIndex);
               }}
             >
-              {props.products.map((product, i) => {
+              {selectedProducts.map((product, i) => {
                 return (
                   <SwiperSlide
                     key={"product-slide" + i}
@@ -83,12 +178,37 @@ const ProductPage: React.FC<Props> = (props) => {
 
           <SlidePagination
             currentIndex={currentIndex}
-            products={props.products}
+            products={selectedProducts}
             callback={(i: number) => {
               changeCurrentSlide(i);
             }}
           ></SlidePagination>
         </div>
+      </div>
+
+      <div className="search-product__wrapper">
+        <SearchProductBar
+          skills={storeSkills.value}
+          isOpen={isSideBarOpen}
+          decide_button_callback={(selectedItems) => {
+            handleDecideButtonClick(selectedItems);
+          }}
+        ></SearchProductBar>
+        <GopherImage
+          callback={() => {
+            setIsSideBarOpen(!isSideBarOpen);
+          }}
+        ></GopherImage>
+        <div
+          onClick={() => {
+            setIsSideBarOpen(false);
+          }}
+          className={
+            "search-product__background" +
+            " " +
+            (isSideBarOpen ? "--active" : "")
+          }
+        ></div>
       </div>
     </Fade>
   );
