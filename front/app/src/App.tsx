cd225a00892565f6b684ed3cbcd95d7de215fe7c
ikkei12 <ikkei12.h@gmail.com>
2020-08-25 10:41:38 +0900

    fix routing

--- front/app/src/App.tsx
+++ front/app/src/App.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
 import Layout from "./components/Layout";
 import AdminSignIn from "./pages/admin/SignIn";
 import AdminHome from "./pages/admin/Home";
@@ -29,29 +33,28 @@ const App: React.FC<PageProps> = () => {
         <Loading isLoaded={isLoaded}></Loading>
       ) : ( */}
       <Router>
-        <Route path="/" component={Home} isLoaded={isLoaded}></Route>
-        <Route
-          path="/profile"
-          component={ProfilePage}
-          isLoaded={isLoaded}
-        ></Route>
-        <Route
-          path="/product"
-          component={ProductPage}
-          isLoaded={isLoaded}
-        ></Route>
-        <Route
-          path="/contact"
-          component={ContactPage}
-          isLoaded={isLoaded}
-        ></Route>
-      </Router>
-      <Router>
+        <Layout current_page={window.location.pathname}>
+          <Route exact path="/" component={Home} isLoaded={isLoaded}></Route>
+          <Route
+            path="/profile"
+            component={ProfilePage}
+            isLoaded={isLoaded}
+          ></Route>
+          <Route
+            path="/product"
+            component={ProductPage}
+            isLoaded={isLoaded}
+          ></Route>
+          <Route
+            path="/contact"
+            component={ContactPage}
+            isLoaded={isLoaded}
+          ></Route>
+        </Layout>
         <Route path="/admin/signIn" component={AdminSignIn}></Route>
         <Route exact path="/admin" component={AdminHome}></Route>
         <Route path="/admin/product" component={AdminProduct}></Route>
       </Router>
-      {/* )} */}
     </div>
   );
 };
--- front/app/src/pages/home/Home.tsx
+++ front/app/src/pages/home/Home.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import axios from "../../plugin/axios/index";
-import Layout from "../../components/Layout";
-import { BrowserRouter as Router, RouteComponentProps } from "react-router-dom";
 
 const Fade = require("react-reveal/Fade");
 
-type Props = {} & RouteComponentProps<{ mode: string }>;
-
-const HOME: React.FC<Props> = (props) => {
+const HOME: React.FC = () => {
   const getData = async () => {
     await axios
       .get("/contributions", {
@@ -18,18 +14,16 @@ const HOME: React.FC<Props> = (props) => {
       });
   };
   return (
-    <Layout current_page={props.match.params.mode}>
-      <Fade bottom delay={500}>
-        <p>home</p>
-        <button
-          onClick={async () => {
-            await getData();
-          }}
-        >
-          click
-        </button>
-      </Fade>
-    </Layout>
+    <Fade bottom delay={500}>
+      <p>home</p>
+      <button
+        onClick={async () => {
+          await getData();
+        }}
+      >
+        click
+      </button>
+    </Fade>
   );
 };
 
