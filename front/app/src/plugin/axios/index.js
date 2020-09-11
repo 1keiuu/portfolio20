aa11cd312cd2c9edbb048aabd8ff7cd98d78da65
ikkei12 <ikkei12.h@gmail.com>
2020-09-11 22:24:25 +0900

    set no-cache

--- front/app/src/plugin/axios/index.js
+++ front/app/src/plugin/axios/index.js
@@ -1,7 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
 const AXIOS = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  headers: {
+    'Cache-Control': 'no-cache',
+  },
 });
 
 export default AXIOS;
