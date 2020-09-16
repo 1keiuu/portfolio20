54ca9a36d197613415f36851142b5dde17e3b987
ikkei12 <ikkei12.h@gmail.com>
2020-09-16 10:47:44 +0900

    fix scraping

--- scraping/controller/today/update_todays_contribution.go
+++ scraping/controller/today/update_todays_contribution.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -13,9 +14,17 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"github.com/saintfish/chardet"
 	"golang.org/x/net/html/charset"
 )
+
+func Env_load() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	Env_load()
